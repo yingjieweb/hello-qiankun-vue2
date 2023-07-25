5176b25a7316e2325c25a7edce9568662602226f
yingjieweb <1184061078@qq.com>
2023-07-25 22:17:13 +0800

    feat: 🎸 add some config ahout qiankun protocol and render logic

--- src/main.js
+++ src/main.js
@@ -1,10 +1,44 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import VueRouter from 'vue-router';
+import routes from './router'
+import './public-path.js' 
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let router = null;
+let instance = null;
+
+function render(props = {}) {
+  const { container } = props;
+  router = new VueRouter({
+    base: window.__POWERED_BY_QIANKUN__ ? '/vue2/' : '/',
+    mode: 'history',
+    routes,
+  });
+
+  instance = new Vue({
+    router,
+    render: (h) => h(App),
+  }).$mount(container ? container.querySelector('#app') : '#app');
+}
+
+// 独立运行时
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+// 作为子应用运行时 -> 暴露 qiankun 协议
+export async function bootstrap() {
+  console.log('[vue2] vue2 app bootstraped');
+}
+export async function mount(props) {
+  console.log('[vue2] props from main framework', props);
+  render(props);
+}
+export async function unmount() {
+  instance.$destroy();
+  instance.$el.innerHTML = '';
+  instance = null;
+  router = null;
+}
